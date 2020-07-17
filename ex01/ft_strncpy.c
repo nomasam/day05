e476ba3d1f713aab0b0ec6e38d3187c30e00ef74
Nomasam Mbanjwa <nombanjw@c4r16s6.wethinkcode.co.za>
2020-07-17 10:02:52 +0200

    ft_strncpy.c

---
+++ ex01/ft_strncpy.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+char *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+ int i;
+
+	i = 0;
+	while (src != '\0' && (i < n))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
