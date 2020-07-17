afb25a1a41d38e92d775e19a2757db31a0d46d70
Nomasam Mbanjwa <nombanjw@c4r16s6.wethinkcode.co.za>
2020-07-17 09:52:30 +0200

    ft_strcpy

---
+++ ex00/ft_strcpy.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+void    ft_putstr(char *str)
+{
+    int i;
+
+    i = 0;
+    while (*(str + i) != '\0')
+    {
+        putchar(*(str + i));
+        i++;
+    }
+}
+/*
+int main()
+{
+  ft_putstr("94sdlg");
+  return 0;
+}*/
